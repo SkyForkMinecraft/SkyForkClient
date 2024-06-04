36262837790df0537ab2a32b0bd10efddb929c5f
LangYa466 <langya466@gmail.com>
2024-06-04 14:58:36 +0800

    版本号

--- src/java/org/union4dev/base/Access.java
+++ src/java/org/union4dev/base/Access.java
@@ -34,7 +34,7 @@ import java.util.regex.Pattern;
  */
 public final class Access {
 
-    public static final String CLIENT_VERSION = "2.4";
+    public static final String CLIENT_VERSION = "2.3";
     public static String CLIENT_NAME = "SkyFork-Client";
     public static Color CLIENT_COLOR = new Color(205,189,255);
     public static boolean loaded;
